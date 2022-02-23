99d656ef0555356106977f9d271fd757ebd7cc42
DumboJ <dumbojxx@gmail.com>
2022-02-24 00:38:17 +0800

    init controller

--- src/main/java/com/dumbo/app/controller/TestController.java
+++ src/main/java/com/dumbo/app/controller/TestController.java
@@ -1,2 +1,12 @@
-package com.dumbo.app.controller;public class TestController {
+package com.dumbo.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+   @GetMapping("/init")
+    public String initCon(){
+       return "dumbo platfrom init";
+   }
 }
