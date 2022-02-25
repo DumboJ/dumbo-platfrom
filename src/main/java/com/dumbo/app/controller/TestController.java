84f2c78ff6534b4320bd5eed1e64d7efda9f6f79
DumboJ <dumbojxx@gmail.com>
2022-02-25 23:58:34 +0800

    delete mvn

--- src/main/java/com/dumbo/app/controller/TestController.java
+++ src/main/java/com/dumbo/app/controller/TestController.java
@@ -1,12 +1,20 @@
 package com.dumbo.app.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/visit")
 public class TestController {
-   @GetMapping("/init")
+   @RequestMapping(value = "/init",method = RequestMethod.GET)
     public String initCon(){
        return "dumbo platfrom init";
+   }
+   @RequestMapping(value = "getList",method = RequestMethod.DELETE)
+    public void del(){
+       System.out.println("del");
    }
 }
