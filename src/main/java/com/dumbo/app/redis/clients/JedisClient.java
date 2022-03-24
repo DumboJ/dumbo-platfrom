b6453cf0f9d884d0298e4418a7246d1c34d1900a
DumboJ <dumbojxx@gmail.com>
2022-03-24 22:20:50 +0800

    add rocketmq konwledge etc..

--- src/main/java/com/dumbo/app/redis/clients/JedisClient.java
+++ src/main/java/com/dumbo/app/redis/clients/JedisClient.java
@@ -5,15 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 @RestController
-@RequestMapping("/jedisSet")
+@RequestMapping(value = "/jedisSet")
 public class JedisClient {
-    public void doWithJedisSingleton(){
-        JedisPool pool = new JedisPool("192.168.5.101",
-                63779
-                );
-        try (
-                Jedis jedis = pool.getResource()) {
+    @RequestMapping(value = "authSet")
+    public String doWithJedisSingleton(){
+        Jedis jedis = null;
+        try {
+            jedis = new Jedis("192.168.5.101", 6379);
+                jedis.auth("dumboj123");
             jedis.set("clientName", "Jedis");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            jedis.close();
         }
+        return "success";
     }
 }
---
+++ src/main/java/com/dumbo/app/rocketmq/producer/AsyncProducerMsg.java
@@ -0,0 +1,9 @@
+package com.dumbo.app.rocketmq.producer;
+/**
+ * 生产 异步消息
+ * */
+public class AsyncProducerMsg {
+    public static void main(String[] args) {
+
+    }
+}
