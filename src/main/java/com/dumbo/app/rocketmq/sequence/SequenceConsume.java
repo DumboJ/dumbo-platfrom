ed65f5252686c06836bef4ff952162d4457516dc
DumboJ <dumbojxx@gmail.com>
2022-03-27 23:46:49 +0800

    sequence message produce and comsunme class

---
+++ src/main/java/com/dumbo/app/rocketmq/sequence/SequenceConsume.java
@@ -0,0 +1,9 @@
+package com.dumbo.app.rocketmq.sequence;
+/**
+ * 指定消息消费顺序
+ * */
+public class SequenceConsume {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/dumbo/app/rocketmq/sequence/SequenceProducer.java
@@ -0,0 +1,9 @@
+package com.dumbo.app.rocketmq.sequence;
+/**
+ * 指定消息生产顺序
+ * */
+public class SequenceProducer {
+    public static void main(String[] args) {
+
+    }
+}
