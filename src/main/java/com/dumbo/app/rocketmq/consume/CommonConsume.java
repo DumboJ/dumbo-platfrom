c2f257021c427a4bf8404684fa16a1c5beed374d
DumboJ <dumbojxx@gmail.com>
2022-04-04 23:51:58 +0800

    usual producer and consume

--- src/main/java/com/dumbo/app/rocketmq/consume/CommonConsume.java
+++ src/main/java/com/dumbo/app/rocketmq/consume/CommonConsume.java
@@ -31,5 +31,22 @@ public class CommonConsume {
         });
         consumer.start();
         System.out.printf("Comsume started. %n");
+       /* //初始化消费者客户端
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer-test");
+        //设置NameServer
+        consumer.setNamesrvAddr("192.168.5.101:9876;192.168.5.102:9876;192.168.5.103:9876");
+        //订阅topic-tag
+        consumer.subscribe("producer","*");
+        //注册消息监听，从broker拉取消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                System.out.printf("%s consumer msg info:%s %n"+Thread.currentThread().getName(),msgs);
+                //标记消息消费成功
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        consumer.start();
+        System.out.printf("Consume started %n");*/
     }
 }
--- src/main/java/com/dumbo/app/rocketmq/producer/SyncProducerMsg.java
+++ src/main/java/com/dumbo/app/rocketmq/producer/SyncProducerMsg.java
@@ -5,28 +5,27 @@ import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 /**
  * 生产 同步消息
  * */
 public class SyncProducerMsg {
-    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException {
-        //实例化消息生产者producer
+    public static void main(String[] args) throws MQClientException, RemotingException, InterruptedException, MQBrokerException, UnsupportedEncodingException {
+        //实例化客户端
         DefaultMQProducer producer = new DefaultMQProducer("producer-test");
-        //NameServer地址
+        //NameServer 地址
         producer.setNamesrvAddr("192.168.5.101:9876;192.168.5.102:9876;192.168.5.103:9876");
-        //启动Producer实例
+        //启动producer实例
         producer.start();
-        for (int i = 0; i < 100; i++) {
-            //创建消息，指定Topic，Tag和消息体
-            Message msg = new Message("producer", "sync-msg", ("RocketMQ producer msg:" + i).getBytes(StandardCharsets.UTF_8));
-            //发送消息到一个Broker
-            SendResult sendResult = producer.send(msg);
-            //SendResult返回消息是否发送成功
-            System.out.printf("%s%n",sendResult);
+        for (int i = 0; i < 200; i++) {
+            Message msg = new Message("producer", "sync-msg", ("RocketMQ producer send msg" + i).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult send = producer.send(msg);
+            System.out.printf("%s%n",send);
         }
         producer.shutdown();
     }
