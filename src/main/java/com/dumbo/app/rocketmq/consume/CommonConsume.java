090b7521c1c0f1fb66cb890c812ad95551eaa920
DumboJ <dumbojxx@gmail.com>
2022-04-04 23:55:25 +0800

    usual producer and consume

--- src/main/java/com/dumbo/app/rocketmq/consume/CommonConsume.java
+++ src/main/java/com/dumbo/app/rocketmq/consume/CommonConsume.java
@@ -14,24 +14,7 @@ import java.util.List;
  * */
 public class CommonConsume {
     public static void main(String[] args) throws MQClientException {
-        //实例化消费者
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consume-test");
-        //设置nameserver
-        consumer.setNamesrvAddr("192.168.5.101:9876;192.168.5.102:9876;192.168.5.103:9876");
-        //订阅一个或者多个topic/tag来过滤需要消费的消息
-        consumer.subscribe("producer", "*");
-        //注册回调处理从broker拉取的消息
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
-            @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-                System.out.printf("%s consume new msg:%s %n",Thread.currentThread().getName(),msgs);
-                //标记消息被成功消费
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-            }
-        });
-        consumer.start();
-        System.out.printf("Comsume started. %n");
-       /* //初始化消费者客户端
+        //初始化消费者客户端
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer-test");
         //设置NameServer
         consumer.setNamesrvAddr("192.168.5.101:9876;192.168.5.102:9876;192.168.5.103:9876");
@@ -41,12 +24,12 @@ public class CommonConsume {
         consumer.registerMessageListener(new MessageListenerConcurrently() {
             @Override
             public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-                System.out.printf("%s consumer msg info:%s %n"+Thread.currentThread().getName(),msgs);
+                System.out.printf("%s consumer msg info:%s %n",Thread.currentThread().getName(),msgs);
                 //标记消息消费成功
                 return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             }
         });
         consumer.start();
-        System.out.printf("Consume started %n");*/
+        System.out.printf("Consume started %n");
     }
 }
